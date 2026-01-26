5d0dbbc6c0fca837e617cd384364b96bee4a89f5
av-gowtham <gowthamavg10@gmail.com>
2026-01-26 16:07:40 +0530

    404 update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,16 @@
 // app/not-found.tsx
 export default function NotFound() {
     return (
-        <div style={{ padding: 40, textAlign: "center" }}>
+        <div
+            style={{
+                padding: 40,
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the page you are looking for does not exist.</p>
         </div>
