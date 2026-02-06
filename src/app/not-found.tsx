eaf98a1990f4088504c19bad54b159975bf878b6
av-gowtham <gowthamavg10@gmail.com>
2026-02-06 07:48:22 +0530

    updated 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 // app/not-found.tsx
 export default function NotFound() {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
-        </div>
+        <main className="404 not-found">
+            <section className="widget-wrapper not-found">
+                <div className="image-content-wrapper">
+                    <Image src="https://raw.githubusercontent.com/gowthamavtech/unique-garden-img/main/404-Image.webp" alt="404 construction image" width={1920} height={1080} />
+                </div>
+                <div className="text-content-wrapper">
+                    <h1>Something went wrong!</h1>
+                    <p>The page couldn’t be loaded right now. Please try again later.</p>
+                    <a href="./" className="btn">
+                        Go to home
+                    </a>
+                </div>
+            </section>
+        </main>
     );
 }
