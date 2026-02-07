915b7ee4382ebb1fa1637e405a33674f1ae0ed3e
av-gowtham <gowthamavg10@gmail.com>
2026-02-08 02:03:05 +0530

    update to the next config

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig = {
         ],
     },
     basePath: process.env.NODE_ENV === "production" ? "/unique-garden" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/unique-garden/" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/unique-garden" : "",
 };
 
 module.exports = nextConfig;
