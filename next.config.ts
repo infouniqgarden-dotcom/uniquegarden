c5af664d36367f89371724d7ed690cb8ec2a07dd
av-gowtham <gowthamavg10@gmail.com>
2026-02-15 17:40:55 +0530

    Fix basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,6 @@ const nextConfig = {
             },
         ],
     },
-    basePath: process.env.NODE_ENV === "production" ? "/unique-garden" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/unique-garden" : "",
 };
 
 module.exports = nextConfig;
